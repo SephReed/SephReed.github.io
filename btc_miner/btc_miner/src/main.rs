a42a6fb63ddda945e0424233faefcfd970ec1989
Seph Reed <ShadyWillowCreek@gmail.com>
2017-08-09 02:23:42 -0500

    colors for portfolio

---
+++ btc_miner/btc_miner/src/main.rs
@@ -0,0 +1,13 @@
+extern crate crypto;
+
+fn main() {
+    // let mut sha256 = Sha256::new();
+    // sha256.input_str(input.as_slice());
+    //
+    // for i in range(0i,16) {
+    //     println!("i == {}, hash == {}", i, sha256.result_str());
+    //     let bytes = sha256.result_bytes();
+    //     sha256.reset();
+    //     sha256.input(bytes.as_slice());
+    // }
+}
---
+++ btc_miner/btc_miner/target/debug/btc_miner.d
@@ -0,0 +1,1 @@
+/Users/seph/Code/Web/SephReed.github.io/btc_miner/btc_miner/target/debug/btc_miner: /Users/seph/Code/Web/SephReed.github.io/btc_miner/btc_miner/src/main.rs
